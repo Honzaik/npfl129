e4182f3ce0f572849449ca0f17431388740fb348
honzaik <honzaik@seznam.cz>
2021-01-22 19:58:39 +0100

    kmeans

--- labs/11/kmeans.py
+++ labs/11/kmeans.py
@@ -7,13 +7,13 @@ import sklearn.datasets
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--clusters", default=3, type=int, help="Number of clusters")
+parser.add_argument("--clusters", default=7, type=int, help="Number of clusters")
 parser.add_argument("--examples", default=200, type=int, help="Number of examples")
-parser.add_argument("--init", default="random", type=str, help="Initialization (random/kmeans++)")
-parser.add_argument("--iterations", default=20, type=int, help="Number of kmeans iterations to perfom")
+parser.add_argument("--init", default="kmeans++", type=str, help="Initialization (random/kmeans++)")
+parser.add_argument("--iterations", default=5, type=int, help="Number of kmeans iterations to perfom")
 parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
-parser.add_argument("--seed", default=42, type=int, help="Random seed")
+parser.add_argument("--seed", default=67, type=int, help="Random seed")
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 def plot(args, iteration, data, centers, clusters):
@@ -50,13 +50,65 @@ def main(args):
     #     generator.choice(unused_points_indices, p=square_distances / np.sum(square_distances))
     #   Use the `np.linalg.norm` to measure the distances.
 
+    if args.init == 'random':
+        centers = data[generator.choice(len(data), size=args.clusters, replace=False)]
+    if args.init == 'kmeans++':
+        centers = []
+        chosenIndices = []
+        for k in range(args.clusters):
+            indices = list(range(len(data)))
+            if k == 0:
+                chosenIndex = generator.randint(len(data))
+            else:
+                square_distances = np.zeros(len(data))
+                for index in indices:
+                    if index in chosenIndices:
+                        square_distances[index] = 0
+                    else:
+                        smallestDistance = np.Inf
+                        for center in centers:
+                            dist = np.linalg.norm(center - data[index])
+                            if (dist < smallestDistance):
+                                smallestDistance = dist
+                        square_distances[index] = smallestDistance ** 2
+
+                chosenIndex = generator.choice(indices, p = (square_distances / np.sum(square_distances)))
+                
+            centers.append(data[chosenIndex])
+            chosenIndices.append(chosenIndex)
+    centers = np.array(centers)
+
     if args.plot:
         plot(args, 0, data, centers, clusters=None)
+
+    clusters = np.zeros(shape=(data.shape[0]))
 
     # Run `args.iterations` of the K-Means algorithm.
     for iteration in range(args.iterations):
         # TODO: Perform a single iteration of the K-Means algorithm, storing
         # zero-based cluster assignment to `clusters`.
+        zs = np.zeros(shape=(data.shape[0], args.clusters))
+
+        for i in range(data.shape[0]):
+            clusterIndex = -1
+            smallestValue = np.Inf
+            for k in range(centers.shape[0]):
+                norm = np.linalg.norm(data[i] - centers[k]) ** 2
+                if (norm < smallestValue):
+                    smallestValue = norm
+                    clusterIndex = k
+
+            zs[i][clusterIndex] = 1
+            clusters[i] = clusterIndex
+
+        for k in range(centers.shape[0]):
+            centers[k] = 0
+            total = 0
+            for i in range(data.shape[0]):
+                total += zs[i][k]
+                if zs[i][k] == 1:
+                    centers[k] += data[i]
+            centers[k] /= total 
 
         if args.plot:
             plot(args, 1 + iteration, data, centers, clusters)
